6863147f3bfc83ed6728905d3e6a03e9f15a46ab
Assaduzzaman Assad <a2z.ice@gmail.com>
2020-03-30 17:26:39 +0600

    added recaptcha dependency

---
+++ src/main/java/com/pluralsight/security/service/GoogleRecaptchaService.java
@@ -0,0 +1,44 @@
+package com.pluralsight.security.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.pluralsight.security.model.RecaptchaDto;
+
+@Service
+public class GoogleRecaptchaService {
+
+	private static final String VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify"
+			+ "?secret={secret}&remoteip={remoteip}&response={response}";
+	private final RestTemplate restTemplate;
+
+	@Value("${recaptcha.secret.key}")
+	private String secretKey;
+	
+	public GoogleRecaptchaService(RestTemplate restTemplate) {
+		this.restTemplate=restTemplate;
+	}
+	
+	public RecaptchaDto verify(String ip, String recaptchaResponse) {
+		Map<String, String> request = new HashMap<>();
+		request.put("remoteip", ip);
+		request.put("secret", secretKey);
+		request.put("response", recaptchaResponse);
+		ResponseEntity<Map> response = restTemplate.getForEntity(VERIFY_URL, Map.class, request);
+		Map<String, Object> body = response.getBody();
+		boolean success = (Boolean)body.get("success");
+		RecaptchaDto recaptchaDto = new RecaptchaDto();
+		recaptchaDto.setSuccess(success);
+		if(!success) {
+		      recaptchaDto.setErrors((List)body.get("error-codes"));
+		}
+		return recaptchaDto;
+	}
+	
+}
